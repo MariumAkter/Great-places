dcd6f84b23da371d6e3e8a6214337cc4ef700109
Mariumchonmon <Mariumakterch@gmail.com>
2021-08-17 19:41:18 +0600

    . Storing the Location in SQLite

--- lib/helpers/location_helper.dart
+++ lib/helpers/location_helper.dart
@@ -1,7 +1,17 @@
-const GOOGLE_API_KEY = 'AIzaSyBrhzobJjFgzoUKkp5T5iRc9DOmCQclAoM';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+const GOOGLE_API_KEY =  'AIzaSyBrhzobJjFgzoUKkp5T5iRc9DOmCQclAoM';
 
 class LocationHelper {
-  static String generateLocationPreviewImage({required double latitude, required double longitude,}) {
+  static String generateLocationPreviewImage({double? latitude, double? longitude,}) {
     return 'https://maps.googleapis.com/maps/api/staticmap?center=&$latitude,$longitude&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$latitude,$longitude&key=$GOOGLE_API_KEY';
+  }
+
+  static Future<String> getPlaceAddress(double? lat, double? lng) async {
+    final url = Uri.parse('https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=$GOOGLE_API_KEY');
+    final response = await http.get(url);
+    return json.decode(response.body)['results'][0]['formatted_address'];
   }
 }
