f36edab96f74b828fede03673e33006191601832
Mariumchonmon <Mariumakterch@gmail.com>
2021-08-17 17:33:19 +0600

    Allowing Users to Pick a Location on the Map

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
-class LocationInput extends StatefulWidget {
 
+import '../helpers/location_helper.dart';
+import '../screens/map_screen.dart';
+
+class LocationInput extends StatefulWidget {
   @override
   _LocationInputState createState() => _LocationInputState();
 }
@@ -11,43 +15,75 @@ class _LocationInputState extends State<LocationInput> {
 
   Future<void> _getCurrentUserLocation() async {
     final locData = await Location().getLocation();
-    print(locData.latitude);
-    print(locData.longitude);
-
+    final staticMapImageUrl = LocationHelper.generateLocationPreviewImage(
+      latitude: locData.latitude!,
+      longitude: locData.longitude!,
+    );
+    setState(() {
+      _previewImageUrl = staticMapImageUrl;
+    });
   }
+
+  Future<void> _selectOnMap() async {
+    final  selectedLocation = await Navigator.of(context).push<LatLng?>(
+      MaterialPageRoute(
+        fullscreenDialog: true,
+        builder: (ctx) => MapScreen(
+          isSelecting: true,
+        ),
+      ),
+    );
+    if (selectedLocation == null) {
+      return;
+    }
+    print(selectedLocation.latitude);
+    // ...
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(children: <Widget> [
-      Container(height: 170,
-      width: double.infinity,
-      alignment: Alignment.center,
-      decoration: BoxDecoration(border: Border.all(width: 1, color: Colors.grey),
-      ),
-      child: _previewImageUrl == null ? Text('No Location chosen',
-      textAlign: TextAlign.center,
-      )
-      : Image.network(
-        _previewImageUrl!,
-        fit: BoxFit.cover,
-      width: double.infinity,),
-      ),
-      Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget> [
-        FlatButton.icon(
-            icon: Icon(Icons.map),
-            label: Text('Select on Map'),
-            textColor: Theme.of(context).primaryColor,
-            onPressed: _getCurrentUserLocation,
+    return Column(
+      children: <Widget>[
+        Container(
+          height: 170,
+          width: double.infinity,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            border: Border.all(width: 1, color: Colors.grey),
+          ),
+          child: _previewImageUrl == null
+              ? Text(
+            'No Location Chosen',
+            textAlign: TextAlign.center,
+          )
+              : Image.network(
+            _previewImageUrl!,
+            fit: BoxFit.cover,
+            width: double.infinity,
+          ),
         ),
-        FlatButton.icon(
-          onPressed: (){},
-          icon: Icon(Icons.location_on),
-          label: Text('Current Location'),
-          textColor: Theme.of(context).primaryColor,
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FlatButton.icon(
+              icon: Icon(
+                Icons.location_on,
+              ),
+              label: Text('Current Location'),
+              textColor: Theme.of(context).primaryColor,
+              onPressed: _getCurrentUserLocation,
+            ),
+            FlatButton.icon(
+              icon: Icon(
+                Icons.map,
+              ),
+              label: Text('Select on Map'),
+              textColor: Theme.of(context).primaryColor,
+              onPressed: _selectOnMap,
+            ),
+          ],
         ),
-      ],)
-    ],
+      ],
     );
   }
 }
